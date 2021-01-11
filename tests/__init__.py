a1a0d9f32ed1e52c0f5a01a01d2a8561709a9069
Martin Kirchgessner <martin.kirch@gmail.com>
2021-01-11 22:19:24 +0100

    fix tests setup

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,21 +3,17 @@ from webtest import TestApp
 from sqlalchemy import create_engine
 import bottle
 from bottle.ext import sqlalchemy
-from showergel import app, Base
+from showergel import app, Base, rest
+
+# you may add echo=True :
+engine = create_engine("sqlite:///:memory:")
+Base.metadata.create_all(engine)
+app.install(sqlalchemy.Plugin(engine))
+
+bottle.debug(mode=True)
 
 class ShowergelTestCase(TestCase):
 
-    def tearDown(self):
-        self.app.reset()
-
     @classmethod
     def setUpClass(cls):
-        from showergel import rest
-
-        # you may add echo=True :
-        engine = create_engine("sqlite:///:memory:")
-        Base.metadata.create_all(engine)
-        app.install(sqlalchemy.Plugin(engine))
-        
-        bottle.debug(mode=True)
         cls.app = TestApp(app)
