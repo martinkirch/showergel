a5138df59a61e561b60ea0723983b3af6dd3d2e7
Martin Kirchgessner <martin.kirch@gmail.com>
2021-02-18 22:18:44 +0100

    update built front

--- showergel/www/js/chunk-2d22d746.0e8dfdbd.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,e,c){"use strict";c.r(e);var t=c("7a23"),i={class:"about"},o=Object(t["d"])("h1",null,"This is Showergel",-1),r=Object(t["d"])("p",null,"pour copyright and licensing here.",-1);function s(n,e){return Object(t["m"])(),Object(t["c"])("div",i,[o,r])}const u={};u.render=s;e["default"]=u}}]);
-//# sourceMappingURL=chunk-2d22d746.0e8dfdbd.js.map
