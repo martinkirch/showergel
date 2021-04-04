862c3a214ee049ed6f41e34d6a0d4f48e0c4bf74
Martin Kirchgessner <martin.kirch@gmail.com>
2021-04-04 13:00:59 +0200

    compilable notifications.js

--- front/src/notifications.js
+++ front/src/notifications.js
@@ -1,8 +1,9 @@
 export default {
   error_handler: function (error) {
-    console.log(error);
     if (error.response && error.response.data.message) {
-      console.log(error.response.data.message)
+      console.log(error.response.data.message);
+    } else {
+      console.log(error);
     }
   }
 }
