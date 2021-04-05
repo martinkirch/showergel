2aeb741c76c6def39bfc0bf5b5ed1ea1d2512bf2
Martin Kirchgessner <martin.kirch@gmail.com>
2021-04-05 17:40:40 +0200

    clearer log message

--- front/src/http.js
+++ front/src/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 if (process.env.NODE_ENV === 'development') {
-  console.log(process.env.VUE_APP_BACKEND_URL)
+  console.log("VUE_APP_BACKEND_URL: "+process.env.VUE_APP_BACKEND_URL)
   if (process.env.VUE_APP_BACKEND_URL) {
     axios.defaults.baseURL = process.env.VUE_APP_BACKEND_URL
   } else {
