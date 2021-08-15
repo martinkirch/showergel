aee14d4f96a89ced2af20f91f3bc8c897312de10
Martin Kirchgessner <martin.kirch@gmail.com>
2021-08-15 23:57:17 +0200

    towards 0.3

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ copyright = '2021, Martin Kirchgessner'
 author = 'Martin Kirchgessner'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2'
+release = '0.3.0-alpha.0'
 
 
 # -- General configuration ---------------------------------------------------
