3e9354cce2f217f48c5f73196af98dd1ef93da1e
Martin Kirchgessner <martin.kirch@gmail.com>
2022-11-13 14:32:14 +0100

    separate the Schedule vue in subcomponents

--- front/src/views/Schedule.vue
+++ front/src/views/Schedule.vue
@@ -1,52 +1,6 @@
 <template>
   <div id="schedule" class="content my-4">
-    <form @submit.prevent="addEvent()" class="box">
-      <h2>Add an event</h2>
-      <div class="field">
-        <label class="label" for="template">Command template</label>
-        <div class="select">
-          <select v-model="template" id="template">
-            <option selected disabled value="">Pick a command... </option>
-            <option v-for="command in parameters.commands" :key="command" :value="command">
-              {{ command }}
-            </option>
-          </select>
-        </div>
-      </div>
-      <div class="field">
-        <label class="label" for="command">Command</label>
-          <div class="control">
-            <input
-              type="text"
-              class="input"
-              v-model="command"
-            />
-          </div>
-      </div>
-      <label class="label" for="day">When</label>
-      <div class="field has-addons">
-        <div class="control">
-          <Datepicker
-            class="input"
-            placeholder="2021-01-01"
-            v-model="day"
-          />
-        </div>
-        <div class="control">
-          <input
-            type="text"
-            class="input"
-            size="8"
-            v-model="time"
-          />
-        </div>
-      </div>
-      <div class="field">
-        <button :class="`button is-link ${isLoading ? 'is-loading' : ''}`" type="submit">
-          Add event
-        </button>
-      </div>
-    </form>
+    <event-add-generic :parameters="parameters"></event-add-generic>
     <h2>Upcoming events</h2>
     <div v-for="event in results" :key="event.id">
       <p>
@@ -70,57 +24,23 @@
 import http from "@/http";
 import notifications from '@/notifications';
 import { format } from "date-fns";
-import Datepicker from "vue3-datepicker";
+import EventAddGeneric from '../components/EventAddGeneric.vue';
 
 export default {
   props: ['parameters'],
-  components: { Datepicker },
+  components: { EventAddGeneric },
   data() {
     return {
-      template: "",
-      day: new Date(),
-      time: format(new Date(), "HH:mm:ss"),
-      command: "",
       results: null,
       isLoading: true,
-      isError: false,
     };
-  },
-  watch: {
-    template(selectedTemplate) {
-      this.command = selectedTemplate;
-    }
   },
   methods: {
     onError(error) {
-      this.isError = true;
       notifications.error_handler(error);
-    },
-    addEvent() {
-      let when = new Date(this.day);
-      let splitted = this.time.split(':');
-      let hour = parseInt(splitted[0]);
-      if (hour || hour === 0) {
-        when.setHours(hour);
-      }
-      let minute = parseInt(splitted[1]);
-      if (minute || minute === 0) {
-        when.setMinutes(minute);
-      }
-      let second = parseInt(splitted[2]);
-      if (second || second === 0) {
-        when.setSeconds(second);
-      }
-      http.put('/schedule', {
-          command: this.command,
-          when: when.toISOString(),
-        })
-          .then(this.getSchedule)
-          .catch(this.onError);
     },
     getSchedule() {
       this.isLoading = true;
-      this.isError = false;
       http
         .get("/schedule")
         .then(this.onResults)
